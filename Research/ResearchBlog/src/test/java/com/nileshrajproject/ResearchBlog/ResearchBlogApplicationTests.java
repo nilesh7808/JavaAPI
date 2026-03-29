58a8626d73f3e81619cfe252d3ce3c54d279522d
Nilesh Raj <rajnilesh088@gmail.com>
2026-03-29 10:41:23 +0530

    Basic Api Practice

---
+++ Research/ResearchBlog/src/test/java/com/nileshrajproject/ResearchBlog/ResearchBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nileshrajproject.ResearchBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
